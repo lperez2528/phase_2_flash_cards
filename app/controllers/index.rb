cf4934d7b9ce76ac43794262ac79e0c4021011ce
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-11 14:01:57 -0500

    Routing to, and views for, homepage and signup pages.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,25 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+
+post '/login' do
+
+  redirect to '/profile/:user_id'
+end
+
+
+get '/profile/:user_id' do
+
+  erb :profile
+end
+
+get '/signup' do
+
+  erb :signup
+end
+
+post '/create' do
+  
+  
+end
