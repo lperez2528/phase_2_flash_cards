e090b06b496b9209d1019d0a16e05c2685382858
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-11 14:22:05 -0500

    adding navbar and profile views, a logout link,..

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,26 +1,24 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
-
 post '/login' do
-
-  redirect to '/profile/:user_id'
-end
-
-
-get '/profile/:user_id' do
-
-  erb :profile
+  redirect to '/profile/:user_name'
 end
 
 get '/signup' do
-
   erb :signup
 end
 
 post '/create' do
-  
-  
+  redirect to '/profile/:user_name'
+end
+
+get '/profile/:user_name' do
+  params[:user_name] = "Paul"
+  @name = params[:user_name]
+  erb :profile
+end
+
+get '/logout' do
 end
